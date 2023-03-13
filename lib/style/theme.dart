f514074771381f8c77050cac21e5bfdba13951e7
sprisao <whdtmek@gmail.com>
2023-03-13 10:55:52 +0900

    ColorsTheme 추가

---
+++ lib/style/theme.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+class ColorTheme {
+  static const Color primary = Color(0xFFFFD771);
+  static const Color primaryDark = Color(0xFFD7A64A);
+  static const Color primaryLight = Color(0xFFFFF5B1);
+
+  static const Color secondary = Color(0xFF7C4DFF);
+  static const Color secondaryDark = Color(0xFF5C2ECC);
+  static const Color secondaryLight = Color(0xFFA17CFF);
+
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color backgroundDark = Color(0xFF000000);
+
+  static const Color bottomNaviBgDark = Color(0xFF17171B);
+
+  static const Color inactiveIcon = Color(0xFFBDBDBD);
+  static const Color inactiveIconDark = Color(0xFF62626C);
+
+  static const Color activeIcon = Color(0xFF1A1E27);
+  static const Color activeIconDark = Color(0xFFFFFFFF);
+
+  static const Color stroke = Color(0xFFE5E8EB);
+
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color line = Color(0xFFE5E8EB);
+}
